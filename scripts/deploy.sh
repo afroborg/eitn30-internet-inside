c410401472f9925fd98b4c066a8a582a999eb529
Axel Froborg <axel.froborg@gmail.com>
2024-03-21 12:19:17 +0100

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 # /bin/bash -e
 
 readonly TARGET_ARCH=aarch64-unknown-linux-gnu
-readonly BINARY_PATH=target/${TARGET_ARCH}/release/$(basename $(pwd))
+readonly BINARY_PATH=target/${TARGET_ARCH}/release/eitn30-internet-inside
 readonly PI_IP=inuti06.lab.eit.lth.se
+readonly SSH_KEY=~/.ssh/eitn30-pi
 
-rsync ${BINARY_PATH} -e "ssh -i ~/.ssh/eitn30-pi" pi@${PI_IP}:~/eitn30
+rsync ${BINARY_PATH} -e "ssh -i ${SSH_KEY}" pi@${PI_IP}:~/eitn30 >/dev/null 2>&1
 echo "Deployed to ${PI_IP}"
