49ed649e7e434283fdca03d65ac05c0ca77a03c9
Axel Froborg <axel.froborg@gmail.com>
2024-03-21 13:30:42 +0100

    test tun setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use std::io::Read;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let mut tun_config = tun::Configuration::default();
+
+    tun_config
+        .address((10, 0, 0, 1))
+        .netmask((255, 255, 255, 0))
+        .up();
+
+    let mut dev = tun::create(&tun_config).unwrap();
+
+    let mut buff = [0u8; 1504];
+
+    loop {
+        let nbytes = dev.read(&mut buff).unwrap();
+        println!("{:?}", &buff[..nbytes]);
+    }
 }
